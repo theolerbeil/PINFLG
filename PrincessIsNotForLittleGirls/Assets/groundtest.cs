2beb5c2d9403f81d60117c194bb00fc643c6d54e
Théo <theolerbeil@gmail.com>
2017-09-28 21:57:44 -0400

    ajout de l'animation Sidewalk + sol et script de test

---
+++ PrincessIsNotForLittleGirls/Assets/groundtest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class groundtest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.Translate (Vector3.forward * 0.15f);
+	}
+}
