e41046454c8909df0beba42703957bce459cbc0e
Lucas <kitolex.soluce@gmail.com>
2017-09-30 18:17:39 -0400

    test des fonctionalité : déplacement + caméra , interaction avec objet , PV

---
+++ PrincessIsNotForLittleGirls/Assets/SCRIPTS/Princesse/princesse_deplacement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class princesse_deplacement : MonoBehaviour {
+
+	public float vitesse;
+	public float force_saut;
+
+	private Rigidbody rb;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		bool saut = Input.GetKeyDown (KeyCode.Space);
+
+		if (saut) {
+			Debug.Log ("vrgx");
+			rb.AddForce (new Vector3(0.0f, force_saut, 0.0f));
+		}
+
+		transform.position += movement * vitesse;
+	}
+
+
+}
