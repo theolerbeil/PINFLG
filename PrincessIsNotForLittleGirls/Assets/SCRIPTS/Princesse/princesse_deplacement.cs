17501e5b90315cbdda90e1cc8eb424c5afea0b2c
Fiesta87 <jb@pommeret.fr>
2017-10-01 19:26:31 -0400

    Ajustement vitesse déplacement princesse en arrière et sur les côtés

--- PrincessIsNotForLittleGirls/Assets/SCRIPTS/Princesse/princesse_deplacement.cs
+++ PrincessIsNotForLittleGirls/Assets/SCRIPTS/Princesse/princesse_deplacement.cs
@@ -27,17 +27,6 @@ public class princesse_deplacement : MonoBehaviour {
         {
             
         }
-        /*
-        bool toucheAvant = Input.GetKey(KeyCode.Z);
-        bool toucheArriere = Input.GetKey(KeyCode.S);
-        bool toucheGauche = Input.GetKey(KeyCode.Q);
-        bool toucheDroite = Input.GetKey(KeyCode.D);
-
-        if (toucheAvant || toucheArriere || toucheGauche || toucheDroite)
-        {
-            GererDeplacement(toucheAvant, toucheArriere, toucheGauche, toucheDroite);
-        }
-        */
         
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
@@ -93,18 +82,13 @@ public class princesse_deplacement : MonoBehaviour {
 			isGrounded = false;
 		}
     }
-    /*
-    private void GererDeplacement(bool toucheAvant, bool toucheArriere, bool toucheGauche, bool toucheDroite)
+    
+    private void GererDeplacement(float moveHorizontal, float moveVertical)
     {
         float difRotation = cam.transform.rotation.eulerAngles.y - this.transform.rotation.eulerAngles.y;
 
 
 
-        if (toucheAvant)
-        {
-            
-        }
-
         float rotation;
 
         if (difRotation > 180.0f)
@@ -121,45 +105,12 @@ public class princesse_deplacement : MonoBehaviour {
 
         this.transform.Rotate(0.0f, rotation, 0.0f);
 
-        // Vector3 mouvement = this.transform.forward * moveVertical + this.transform.right * moveHorizontal;
+        Vector3 mouvement = this.transform.forward * Mathf.Max(moveVertical, -0.5f);
+        float norme = Mathf.Max(mouvement.magnitude, 0.5f);
 
-        Vector3 mouvement = this.transform.forward;
+        mouvement += this.transform.right * moveHorizontal * 0.5f;
 
-        mouvement.Normalize();
-
-        this.transform.position += mouvement * vitesse;
-    }*/
-
-    
-    private void GererDeplacement(float moveHorizontal, float moveVertical)
-    {
-        float difRotation = cam.transform.rotation.eulerAngles.y - this.transform.rotation.eulerAngles.y;// + (90.0f * moveHorizontal);
-
-
-
-        float rotation;
-
-        if (difRotation > 180.0f)
-        {
-            difRotation -= 360.0f;
-        }
-
-        if (difRotation < -180.0f)
-        {
-            difRotation += 360.0f;
-        }
-
-        rotation = Mathf.Clamp(difRotation, -vitesseAngulaire, vitesseAngulaire);
-
-        this.transform.Rotate(0.0f, rotation, 0.0f);
-
-        Vector3 mouvement = this.transform.forward * moveVertical + this.transform.right * moveHorizontal;
-
-        // Vector3 mouvement = this.transform.forward * moveVertical;
-
-        // Vector3 mouvement = this.transform.forward * Mathf.Min(moveVertical + Mathf.Abs(moveHorizontal), 1.0f);
-
-        mouvement.Normalize();
+        mouvement = (mouvement / mouvement.magnitude) * norme;
 
         this.transform.position += mouvement * vitesse;
     }
