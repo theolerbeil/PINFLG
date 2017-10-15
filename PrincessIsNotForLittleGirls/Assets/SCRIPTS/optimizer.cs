6a5393f8469df3b65a8a5822b0525637a546775a
Théo <theolerbeil@gmail.com>
2017-10-15 15:08:36 -0400

    Ajout d'un premier niveau de test

---
+++ PrincessIsNotForLittleGirls/Assets/SCRIPTS/optimizer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class optimizer : MonoBehaviour {
+	
+	void Update(){
+		
+		//gameObject.SetActive (true);
+	}
+}
---
+++ PrincessIsNotForLittleGirls/Assets/SCRIPTS/optimizer2.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+ var Target : GameObject;
+ 
+ function Update ()
+ {
+
+   //  print ("visible");
+   // Target.SetActive(true);
+ }
+
+   
+
+
+
