5e6bd687232862bb6d9ec322516d3496393ca575
Lucas <kitolex.soluce@gmail.com>
2017-10-28 19:19:20 -0400

    ajout objet de progression

---
+++ PrincessIsNotForLittleGirls/Assets/SCRIPTS/Objets/ObjetProgression.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjetProgression : Objet {
+
+	private princesse_objetProgression princesse;
+	public EnumObjetProgression objetProgression;
+
+	// Use this for initialization
+	void Start () {
+		princesse= GameObject.FindGameObjectWithTag("Player").GetComponent<princesse_objetProgression>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	override
+	public void Activation()
+	{
+		princesse.addItem(objetProgression,this.gameObject);
+	}
+}
