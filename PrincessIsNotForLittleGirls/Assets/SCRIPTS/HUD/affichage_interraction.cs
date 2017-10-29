4fa1787039c8f0aeb450a7223855f75d03c64bdc
Lucas <kitolex.soluce@gmail.com>
2017-10-29 13:23:14 -0400

    différence affichage arme et objet progression HUD

--- PrincessIsNotForLittleGirls/Assets/SCRIPTS/HUD/affichage_interraction.cs
+++ PrincessIsNotForLittleGirls/Assets/SCRIPTS/HUD/affichage_interraction.cs
@@ -4,22 +4,32 @@ using UnityEngine;
 
 public class affichage_interraction : MonoBehaviour {
 
-	private UnityEngine.UI.Image image_detection;
-	public List<Objet> list_objet;
+	private List<Objet> list_objet;
+	private Transform[] listImageInterraction;
 
 	// Use this for initialization
 	void Start () {
-		image_detection=GetComponent< UnityEngine.UI.Image>();
-		image_detection.enabled = false;
+		//image_detection=GetComponent< UnityEngine.UI.Image>();
+		listImageInterraction = gameObject.GetComponentsInChildren<Transform>();
+
+
+	//	image_detection.enabled = false;
 		list_objet = new List<Objet>();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		if (list_objet.Count > 0) {
-			image_detection.enabled = true;
+
+		if (list_objet.Count > 0 ) {
+			Objet objetfutur = list_objet.ToArray ()[ list_objet.ToArray().Length-1];
+			if (objetfutur is Arme) {			
+				afficheObjet ("interraction_"+objetfutur.name);
+			}else if(objetfutur is ObjetProgression){
+				afficheObjet ("interraction_objetProgression");
+			}
+
 		} else {
-			image_detection.enabled = false;
+			desafficheObjet ();
 		}
 	}
 
@@ -31,6 +41,25 @@ public class affichage_interraction : MonoBehaviour {
 
 	public void desactiveObjet(Objet objet){
 		list_objet.Remove (objet);
+	}
+
+	private void afficheObjet(string nomImage){
+		bool trouve = false;
+		foreach(Transform t in listImageInterraction){
+			if (t.name.Equals(nomImage)) {
+				trouve = true;
+				t.GetComponent<UnityEngine.UI.Image> ().enabled = true;
+			}
+		}
+		if (!trouve) {
+			afficheObjet ("interraction_objetProgression");
+		}
+	}
+
+	private void desafficheObjet(){
+		for(int i=1;i<listImageInterraction.Length;i++){
+			listImageInterraction[i].GetComponent<UnityEngine.UI.Image> ().enabled = false;
+		}
 	}
 		
 }
