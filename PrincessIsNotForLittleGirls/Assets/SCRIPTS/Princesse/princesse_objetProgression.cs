7a4c8936542f20112fded2bbb03f84c8a566779a
Lucas <kitolex.soluce@gmail.com>
2017-11-22 15:37:54 -0500

    THEO

--- PrincessIsNotForLittleGirls/Assets/SCRIPTS/Princesse/princesse_objetProgression.cs
+++ PrincessIsNotForLittleGirls/Assets/SCRIPTS/Princesse/princesse_objetProgression.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class princesse_objetProgression : MonoBehaviour {
 
-	private List<EnumObjetProgression> listObjet;
+	public List<EnumObjetProgression> listObjet;
 
 	// Use this for initialization
 	void Start () {
