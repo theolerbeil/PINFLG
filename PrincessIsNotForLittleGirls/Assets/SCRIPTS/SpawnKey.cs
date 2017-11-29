1d619f025f7e46789825dd00fef9ca7f2bce1afa
gduminy <guillaume.duminy@etu.unilim.fr>
2017-11-29 15:20:51 -0500

    Ajout de la clé et apparition quand le troll meurt

---
+++ PrincessIsNotForLittleGirls/Assets/SCRIPTS/SpawnKey.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnKey : MonoBehaviour {
+
+	private mob_vie VieTroll;
+	private GameObject Key;
+	// Use this for initialization
+	void Start () {
+		VieTroll = GetComponent<mob_vie>() ;
+		Key = GameObject.FindGameObjectWithTag ("key");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (VieTroll.estEnVie ()) {
+			Key.SetActive (false);
+		}else{
+			Key.SetActive (true);
+		}
+	}
+}
