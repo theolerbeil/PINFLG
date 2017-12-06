af24e3a77db6af5d42b4b5840f4b54ba1c52b34f
Lucas <kitolex.soluce@gmail.com>
2017-12-06 15:51:37 -0500

    le jeu peut être mis en pause

--- PrincessIsNotForLittleGirls/Assets/SCRIPTS/HUD/affichage_pause.cs
+++ PrincessIsNotForLittleGirls/Assets/SCRIPTS/HUD/affichage_pause.cs
@@ -27,16 +27,16 @@ public class affichage_pause : MonoBehaviour {
 
 	public void Pause(){
 		Time.timeScale = 0;
-		foreach (Transform t in affiche_Pause) {
-			t.gameObject.SetActive (true);
+		for (int i = 1; i < affiche_Pause.Length; i++) {
+			affiche_Pause[i].gameObject.SetActive (true);
 		}
 		etat = false;
 	}
 
 	public void finPause(){
 		Time.timeScale = 1;
-		foreach (Transform t in affiche_Pause) {
-			t.gameObject.SetActive (false);
+		for (int i = 1; i < affiche_Pause.Length; i++) {
+			affiche_Pause[i].gameObject.SetActive (false);
 		}
 		etat = true;
 	}
