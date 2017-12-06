e52bfb7d57ef77f5bed664bd00915e9884e0f152
gduminy <guillaume.duminy@etu.unilim.fr>
2017-12-06 14:46:27 -0500

    La clé disparait aprés l'avoir prise dans la scene 2

--- PrincessIsNotForLittleGirls/Assets/SCRIPTS/SpawnKey.cs
+++ PrincessIsNotForLittleGirls/Assets/SCRIPTS/SpawnKey.cs
@@ -6,18 +6,22 @@ public class SpawnKey : MonoBehaviour {
 
 	private mob_vie VieTroll;
 	private GameObject Key;
+	private bool PremiereFois;
 	// Use this for initialization
 	void Start () {
 		VieTroll = GetComponent<mob_vie>() ;
 		Key = GameObject.FindGameObjectWithTag ("key");
+		PremiereFois = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (VieTroll.estEnVie ()) {
 			Key.SetActive (false);
-		}else{
+			PremiereFois = true;
+		}else if(!VieTroll.estEnVie() && PremiereFois == true){
 			Key.SetActive (true);
+			PremiereFois = false;
 		}
 	}
 }
