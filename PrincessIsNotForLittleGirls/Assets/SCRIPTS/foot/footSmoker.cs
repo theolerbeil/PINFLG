46302e9d457167dad1b409772641d5e1d7c27f6f
Fiesta87 <jb@pommeret.fr>
2017-12-06 16:00:46 -0500

    ajout des effet de poussière lors des pas de la princesse

---
+++ PrincessIsNotForLittleGirls/Assets/SCRIPTS/foot/footSmoker.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class footSmoker : MonoBehaviour {
+
+	public Transform smokeEffect;
+	private bool isTriggered;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other){
+
+		if (!isTriggered && other.tag.Equals ("wall")) {
+
+			Instantiate (smokeEffect, this.transform.position, smokeEffect.transform.rotation);
+		}
+	}
+
+	void OnTriggerExit(Collider other){
+
+		if (isTriggered && other.tag.Equals ("wall")) {
+			isTriggered = false;
+		}
+	}
+}
