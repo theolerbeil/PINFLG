7dc648df11d1e6e297dfe98d8211b3c2d8185508
Lucas <kitolex.soluce@gmail.com>
2017-12-06 14:39:07 -0500

    ajout levier

--- PrincessIsNotForLittleGirls/Assets/SCRIPTS/HUD/affichage_pause.cs
+++ PrincessIsNotForLittleGirls/Assets/SCRIPTS/HUD/affichage_pause.cs
@@ -5,37 +5,39 @@ using UnityEngine;
 public class affichage_pause : MonoBehaviour {
 	
 	private Transform[] affiche_Pause;
+	public bool etat;
 
 
 	// Use this for initialization
 	void Start () {
 		affiche_Pause = gameObject.GetComponentsInChildren<Transform>(true);
+		etat = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetButtonDown ("pause")) {
-			if (Time.timeScale == 0) {
-				desaffichePause ();
-				Time.timeScale = 1;
-
+			if (etat) {
+				Pause ();
 			} else {
-				Time.timeScale = 0;
-				affichePause ();
+				finPause ();
 			}
-
 		}
 	}
 
-	public void affichePause(){
+	public void Pause(){
+		Time.timeScale = 0;
 		foreach (Transform t in affiche_Pause) {
 			t.gameObject.SetActive (true);
 		}
+		etat = false;
 	}
 
-	public void desaffichePause(){
+	public void finPause(){
+		Time.timeScale = 1;
 		foreach (Transform t in affiche_Pause) {
 			t.gameObject.SetActive (false);
 		}
+		etat = true;
 	}
 }
---
+++ PrincessIsNotForLittleGirls/Assets/SCRIPTS/Objets/ObjetEnvironnement/Levier.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Levier : MonoBehaviour {
+
+	public ObjetEnvironnement[] listObjetEnvironnement;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		foreach (ObjetEnvironnement objet in listObjetEnvironnement) {
+			objet.Activation ();
+		}
+	}
+}
